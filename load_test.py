af92a639613a9329b4644e3a8e720922350c5d57
Ridvik Pal <ridvikpal@gmail.com>
2025-11-02 11:15:27 -0500

    Updated load test with box plot

--- load_test.py
+++ load_test.py
@@ -1,7 +1,16 @@
 import time
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import requests
+
+def plot_boxplot(data):
+    plt.boxplot(data)
+    
+    plt.title(f'Latency for Test Messages')
+    plt.ylabel('Latency')
+    plt.xlabel('Test Message')
+
+    plt.savefig('load_test_boxplot.png')
 
 def do_request(session, url, message, timeout=10):
     """Perform a single POST request and measure latency in ms."""
@@ -26,7 +35,7 @@ def do_request(session, url, message, timeout=10):
     return {"message": message, "latency_ms": latency_ms, "status": status, "label": label}
 
 if __name__ == "__main__":
-    base_url = 'http://127.0.0.1:8000'
+    base_url = 'http://127.0.0.1:8000' # modify this!!!
     endpoint = '/predict'
     full_url = base_url + endpoint
 
@@ -53,5 +62,14 @@ if __name__ == "__main__":
     df = pd.DataFrame(results)
     df.index += 1
     df.to_csv('load_test_results.csv', index_label='Req ID')
+
+    latency_data = [result['latency_ms'] for result in results]
+
+    plot_boxplot([
+        latency_data[:100],
+        latency_data[100:200],
+        latency_data[200:300],
+        latency_data[300:]
+    ])
 
     sess.close()
